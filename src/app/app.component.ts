2fb3a628e9a37eacc4d2d4a197b95bb42f05bc00
MotokiNakae <motoki@cleverdog.me>
2021-10-08 15:24:14 +0900

    change active cell style, behavior for key board navigation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IActiveNodeChangeEventArgs, IGridKeydownEventArgs, IgxTreeGridComponent } from 'igniteui-angular';
+import { EMPLOYEE_DATA } from './data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'treegrid-selection';
+  @ViewChild('grid1', { read: IgxTreeGridComponent, static: true })
+  public grid1!: IgxTreeGridComponent;
+  public localData!: any[];
+
+  public ngOnInit() {
+    this.localData = EMPLOYEE_DATA;
+  }
+
+  public customKeydown(args: IActiveNodeChangeEventArgs) {
+    let targetRowID = this.grid1.getRowByIndex(args.row).data.ID;
+    this.grid1.selectRows([targetRowID],true);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { IgxTreeGridModule } from "igniteui-angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    IgxTreeGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
